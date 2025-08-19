930e467e59e0c3772c9624293dd01c2f1500d2ef
pikapika <kataroto2017@gmail.com>
2025-08-19 14:17:14 +0700

    Initial Commit

--- b31doichoTrucTiep.java
+++ b31doichoTrucTiep.java
@@ -1,4 +1,27 @@
-import
+import java.util.*;
 
-public class b31doichoTrucTiep {
+public class b31doichoTrucTiep{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0;i< n - 1;i++){
+            for(int j = i + 1;j < n;j++){
+                if(a[i] > a[j]){
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
+                }
+            }
+            System.out.print("Buoc " +(i + 1) + ": ");
+            for(int k = 0; k < n; k++){
+                System.out.print(a[k] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
