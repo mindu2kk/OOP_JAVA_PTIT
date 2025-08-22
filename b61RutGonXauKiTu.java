e8462d529cb340ddbb2766bd88c39d92431b6d5b
pikapika <kataroto2017@gmail.com>
2025-08-22 23:09:02 +0800

    Initial Commit

---
+++ b61RutGonXauKiTu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class b61RutGonXauKiTu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String str = sc.next();
+
+        }
+    }
+}
