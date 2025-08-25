2fe240a3aea3f5c9a51f134bf5427ca674359a76
pikapika <kataroto2017@gmail.com>
2025-08-25 22:46:50 +0800

    Initial Commit

---
+++ b72KhaiBaoLopNhanVien.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class b72KhaiBaoLopNhanVien{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String ten = sc.nextLine();
+        String gt = sc.nextLine();
+        String ns = sc.nextLine();
+        String dc = sc.nextLine();
+        String mst = sc.nextLine();
+        String hd = sc.nextLine();
+        System.out.println("00001 " + ten + " " + gt + " " + ns + " " + dc + " " + mst + " " + hd);
+        sc.close();
+    }
+}
